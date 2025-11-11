11dd4a6c2e447fdf50c31854a1b945c7507c4206
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-11-11 16:45:54 -0700

    starting the main component of the game. where all the cards are fetched from the api and the logic for clicks are handled.

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react"
+
 export function Gameboard(){
     const [currScore, setCurrScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
+    const [cards, setCards] = useState([{
+        id: 0,
+        name: '',
+        image: '',
+        clicked: false
+    }]);
+
+    useEffect(() => {
+        async function fetchCards() {
+            try{
+                const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=12/');
+                const data = await res.json();//12 cards for the gameboard
+
+            }
+            catch(error){
+            
+            }
+        }
+    })
 
     //add onclick function (determine reset or currScore increase) here or it can be imported from utils
     //along with some shuffle function

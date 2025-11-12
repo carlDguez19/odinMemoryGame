a646dafff61267b6463fc57eb3997fb86ce41c52
Carlos <110745188+carlDguez19@users.noreply.github.com>
2025-11-11 23:56:33 -0700

    set up connection to api to fetch images of pokemons for each card

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -3,25 +3,41 @@ import { useState, useEffect } from "react"
 export function Gameboard(){
     const [currScore, setCurrScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
-    const [cards, setCards] = useState([{
-        id: 0,
-        name: '',
-        image: '',
-        clicked: false
-    }]);
+    const [cards, setCards] = useState([]);
 
     useEffect(() => {
         async function fetchCards() {
             try{
                 const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=12/');
-                const data = await res.json();//12 cards for the gameboard
+                const data = await res.json();//12 cards for the gameboard;
 
+                const cardData = await Promise.all(data.results.map(async (pokemon) => {
+                    const pokemonRes = await fetch(pokemon.url);
+                    const pokemonData = await pokemonRes.json();
+                    return {
+                        id: pokemonData.id,
+                        name: pokemonData.name,
+                        image: pokemonData.sprites.front_default,
+                        clicked: false
+                    };
+                })
+            );
+            setCards(cardData);
             }
             catch(error){
-            
+                console.error("Error fetching card data:", error);
             }
         }
-    })
+        fetchCards();
+    }, []);
+
+    return (
+        <div className="pokeCard">
+            {cards.map((card) => {
+                <img key={pokemon.id} src={card.image} alt={pokemon.name} className="pokeImg" />
+            })}
+        </div>
+    )
 
     //add onclick function (determine reset or currScore increase) here or it can be imported from utils
     //along with some shuffle function
